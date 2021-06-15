e54b04f5785eac5237c101f99517a21ee9e861d2
somkiart jobsungnoen <adab.writer@gmail.com>
2021-06-15 14:04:44 +0700

    add path

--- project-angular-app/src/app/app-routing.module.ts
+++ project-angular-app/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ShowEmployeeComponent } from './components/show-employee/show-employee.component';
+import { ShowMemberComponent } from './components/show-member/show-member.component';
+import { ShowMenuComponent } from './components/show-menu/show-menu.component';
 import { SigninAdminComponent } from './components/signin-admin/signin-admin.component';
 import { SigninEmployeeComponent } from './components/signin-employee/signin-employee.component';
 
 const routes: Routes = [
   {path: 'signin', component: SigninAdminComponent},
   {path: 'signin', component: SigninEmployeeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'show-employee', component: ShowEmployeeComponent},
+  {path: 'show-member', component: ShowMemberComponent},
+  {path: 'show-menu', component: ShowMenuComponent},
   {path: '', 
     redirectTo: 'signin',
     pathMatch: 'full'
--- project-angular-app/src/app/app.module.ts
+++ project-angular-app/src/app/app.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -31,7 +37,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     HttpClientModule,
     AngularWebStorageModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSliderModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
