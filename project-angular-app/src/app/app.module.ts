bd4e35af1ab7502e2781a8f27e4cbdb8550c7129
somkiart jobsungnoen <adab.writer@gmail.com>
2021-06-15 13:41:40 +0700

    add component

--- project-angular-app/src/app/app.module.ts
+++ project-angular-app/src/app/app.module.ts
@@ -8,12 +8,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { AngularWebStorageModule } from 'angular-web-storage';
 import { SigninAdminComponent } from './components/signin-admin/signin-admin.component';
 import { SigninEmployeeComponent } from './components/signin-employee/signin-employee.component';
+import { ShowMemberComponent } from './components/show-member/show-member.component';
+import { ShowEmployeeComponent } from './components/show-employee/show-employee.component';
+import { ShowMenuComponent } from './components/show-menu/show-menu.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninAdminComponent,
-    SigninEmployeeComponent
+    SigninEmployeeComponent,
+    ShowMemberComponent,
+    ShowEmployeeComponent,
+    ShowMenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
