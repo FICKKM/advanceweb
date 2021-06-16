1b1c3394ca4f2f063790526bfad3d710b76732fb
B6116262 <pahnta33860@gmail.com>
2021-06-16 13:18:40 +0700

    panta

--- project-angular-app/src/app/components/add-member/add-member.component.ts
+++ project-angular-app/src/app/components/add-member/add-member.component.ts
@@ -1,17 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-
+import { AddMemberService } from '../../services/add-member.service'
 @Component({
   selector: 'app-add-member',
   templateUrl: './add-member.component.html',
   styleUrls: ['./add-member.component.css']
 })
 export class AddMemberComponent implements OnInit {
+  memberForm = new FormGroup({
+    id: new FormControl('', [Validators.required]),
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    tel: new FormControl('', [Validators.required]),
+    sum_price: new FormControl('', [Validators.required]),
+  });
 
-  constructor(private router: Router) { }
+  previewLoaded: boolean = false;
+  constructor(private addmem: AddMemberService) { }
 
   ngOnInit(): void {
+  }
+  addMember() {
+    this.addmem.addMem(this.memberForm.value).subscribe(
+      data => {
+        console.log(data)
+        alert('Member added successfully');
+        this.memberForm.reset();
+      },
+      err => {
+        console.log(err);
+      });
+  }
+
+  onChangeImg(e: any) {
+    if (e.target.files.length > 0) {
+      const file = e.target.files[0];
+      var pattern = /image-*/;
+      const reader = new FileReader();
+      if (!file.type.match(pattern)) {
+        alert('invalid format');
+        this.memberForm.reset();
+      } else {
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+          this.previewLoaded = true;
+          this.memberForm.patchValue({
+            img: reader.result
+          });
+        };
+      }
+    }
+  }
+
+  resetForm() {
+    this.memberForm.reset();
+    this.previewLoaded = false;
   }
 
 }
---
+++ project-angular-app/src/app/services/add-member.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddMemberService {
+
+  members: any;
+  constructor(private http: HttpClient) { }
+
+  addMem(member) {
+    return this.http.post<any>('http://localhost:4400/members/add', member)
+      .pipe(map(data => {
+        return data;
+      }));
+  }
+  getMem() {
+    return this.http.get<any>('http://localhost:4400/members/get')
+      .pipe(map(data => {
+        if (data) {
+          this.members = data;
+          console.log(this.members);
+        }
+        return this.members;
+      }));
+  }
+
+}
